dd923ec0af896d2ad9111d8c67c6b30e68e41c2b
deskach <deskach@gmail.com>
2023-03-29 09:21:17 +0300

    Add footer

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,14 +5,17 @@ import {ClarityModule} from "@clr/angular";
 import {BrowserModule} from "@angular/platform-browser";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {RouterModule} from "@angular/router";
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    FooterComponent,
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
