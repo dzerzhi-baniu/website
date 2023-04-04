9ad7c053b365c14ce606e478ee5a39e8d8b821f2
deskach <deskach@gmail.com>
2023-04-04 11:02:03 +0300

    Fix address

--- src/app/services/contacts.service.ts
+++ src/app/services/contacts.service.ts
@@ -5,13 +5,21 @@ interface IPhone {
   caption: string
 }
 
+interface IAddress {
+  address: string,
+  url: string,
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ContactsService {
-  public phones: IPhone[] = [ {caption: '+375(25)7122237', number: '+375257122237'}]
+  public phones: IPhone[] = [{caption: '+375(25)7122237', number: '+375257122237'}]
   public emails = ['dzerzhi.baniu@gmail.com']
-  public address = "Дзержинский район, деревня Бокиново."
+  public address: IAddress = {
+    address: "Дзержинский район, деревня Бакиново",
+    url: "https://ru.wikipedia.org/wiki/%D0%91%D0%B0%D0%BA%D0%B8%D0%BD%D0%BE%D0%B2%D0%BE"
+  }
 
   constructor() {
   }
